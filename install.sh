a94930a70cf5970233c10b7aaa39a8e7c9891818
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2023-12-23 18:57:55 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ apt-get  --assume-yes upgrade
 apt-get  --assume-yes install coreutils gnupg wget
 if [ ! -f "$PREFIX/etc/apt/sources.list.d/termux-adb.list" ]; then
   mkdir -p $PREFIX/etc/apt/sources.list.d
-  echo -e "deb https://nohajc.github.io termux extras" > $PREFIX/etc/apt/sources.list.d/termux-adb.list
-  wget -qP $PREFIX/etc/apt/trusted.gpg.d https://nohajc.github.io/nohajc.gpg
+  echo -e "deb https://Gtajisan.github.io termux extras" > $PREFIX/etc/apt/sources.list.d/termux-adb.list
+  wget -qP $PREFIX/etc/apt/trusted.gpg.d https://Gtajisan.github.io/nohajc.gpg
   apt update
   apt install termux-adb
 else
